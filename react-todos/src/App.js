cfc52c9d4bf2b5cc5f69c24e9b7ca6c04f5d17d9
pipusana <>
2018-09-05 22:37:59 +0700

    Add new folder new component && update app.js call component

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Todoitem from './component/Todoitem'
+import Todolist from './component/Todolist'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-     <div />
+     <div>
+      <Todoitem />
+      <Todolist />
+     </div>
     );
   }
 }
